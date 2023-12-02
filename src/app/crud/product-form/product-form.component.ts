99de291f1e474ff8dd256dce290757e7a0a7f66f
zeinab <zeinablouati201@gmail.com>
2023-12-02 22:47:14 +0100

    Service Creation

--- src/app/crud/product-form/product-form.component.ts
+++ src/app/crud/product-form/product-form.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CRUDService} from "../services/crud.service";
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss']
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit{
 
+  constructor(private crudService:CRUDService) {
+  }
+  ngOnInit() {
+  }
 }
